ee3ddc1611d03c75b7480f2ec213d66938c30b25
edq <98626554+emocool@users.noreply.github.com>
2025-10-31 12:06:34 +0900

    関数とエージェントの責務分割

--- agents/team1_agent/flop_strategy_agent/tools/__init__.py
+++ agents/team1_agent/flop_strategy_agent/tools/__init__.py
@@ -1,3 +1,4 @@
 from .flop_tools import evaluate_hand, calculate_equity
+from .analysis import suggest_flop_action
 
-__all__ = ["evaluate_hand", "calculate_equity"]
+__all__ = ["evaluate_hand", "calculate_equity", "suggest_flop_action"]
